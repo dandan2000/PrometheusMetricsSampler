7e44d5696beff138d602043736a43d83b26c328e
Daniel Celentano <daniel.celentano@notpublic.com>
2022-02-20 23:55:34 -0300

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# PrometheusMetricsSampler
+Simplest Java Prometheus Sampler
+
+
+mvn clean package
+
+java -cp target/PrometheusMetricsSampler-1.0-jar-with-dependencies.jar com.dandan2000.metrics.MainClass
+
+
+At any interfaces
+
+http://localhost:9090/metrics
+
+
---
+++ src/main/java/com/dandan2000/metrics/MainClass.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dandan2000.metrics;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+import java.util.Random;
+
+/**
+ *
+ * @author daniel
+ */
+public class MainClass {
+
+    final static int PORT = 9090;
+    
+    public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(PORT), 0);
+        server.createContext("/metrics", new MetricsHandler());
+        server.setExecutor(null); 
+        System.out.println("Starting server at " + PORT);
+        server.start();
+    }
+
+    static class MetricsHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            Random rand = new Random();
+            
+            String response = "# HELP apache_accesses_total Current total apache accesses (*)\n" +
+                "# TYPE apache_accesses_total counter\n" +
+                "apache_accesses_total " + rand.nextInt(100);;
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+
+}
